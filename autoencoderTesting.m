e71b0ba6fa81fba92ea6e264d28d343fe93d1770
martacarraro <marta.carraro.5@studenti.unipd.it>
2021-01-03 20:12:46 +0100

    uso features salvate come input dell'autoencoder

--- autoencoderTesting.m
+++ autoencoderTesting.m
@@ -3,44 +3,24 @@ clc
 clear all
 close all
 
-% datasets
-% dataset_name = 'portello' %training
-% dataset_name = 'tiso' %testing
-% dataset_name = 'castle' %training
-% dataset_name = 'fountain' %testing
+%% Load the training set and the test set
 
-
-
-%% Load the dataset and preprocess the images
-
-[images,cameraParams] = loadGrayData('portello');
-%% processing SURF on train image
-
-for i = 1:length(images)
-    I = undistortImage(images{i}, cameraParams);
-    keypoints{1,i} = detectSURFFeatures(I, 'NumOctaves', 8);
-    features{1,i} = extractFeatures(I, keypoints{1,i}, 'Upright', true,'FeatureSize',128); %try set Upright to false
-end
-
+dataset_name = 'portello'
+% dataset_name = 'castle'
+load(strcat('data/', dataset_name,'/', dataset_name, '_features.mat'))
 train_set = feat2set(features);
 
-
-[images,cameraParams] = loadGrayData('tiso');
-%% processing SURF on test image
-for i = 1:length(images)
-    I = undistortImage(images{i}, cameraParams);
-    keypoints{1,i} = detectSURFFeatures(I, 'NumOctaves', 8);
-    features{1,i} = extractFeatures(I, keypoints{1,i}, 'Upright', true,'FeatureSize',128); %try set Upright to false
-end
+dataset_name = 'tiso'
+% dataset_name = 'fountain'
+load(strcat('data/', dataset_name,'/', dataset_name, '_features.mat'))
 test_set = feat2set(features);
 
 mse_error_train = zeros(1,128);
 mse_error_test = zeros(1,128);
 
-save('data/train&test.mat','train_set','test_set');
 %% autoencoding
-load('data/train&test.mat')
-layers = [64,32,16,8,4,2,1];
+
+layers = [32,16,8,4,2,1];
 GPU_bool = true;
 
 mse_error_train = zeros(1,length(layers));
